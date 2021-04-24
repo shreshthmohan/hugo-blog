06db4d8f3b75481534246932743e88e9ba523f3c
Shreshth Mohan <sremog@gmail.com>
2021-04-24 22:05:05 +0530

    new file: random card

---
+++ static/js/random-card-data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    quote:
+      "You are one person in a game with seven billion other people and infinite moving parts. The accidental impact of actions outdside of your control can be more consequential than the ones you consciously take.",
+    book: "The Psychology of Money",
+    author: "Morgan Housel",
+    link: "https://amzn.to/3sW3qFH",
+    page: 28,
+  },
+  {
+    quote:
+      "You'll get closer to actionable takeaways by looking for broad patterns of success and failure. The more common the pattern, the more applicable it might be to your life.",
+    book: "The Psychology of Money",
+    author: "Morgan Housel",
+    link: "https://amzn.to/3sW3qFH",
+    page: 33,
+  },
+  {
+    quote:
+      "The ability to do what you want, when you want, with who you want, for as long as you want, is priceless. It is the highest dividend money pays.",
+    book: "The Psychology of Money",
+    author: "Morgan Housel",
+    myNotes:
+      "So making money to acheive this level might be enough. You might not need to suffer for more than what you need. The other day I was talking to a friend about what his fuck-you money number was. His number was twice as much as mine. The reason, he wanted to buy a place to be able to run a learning center. But I argued that he needed less as he didn't need to buy the place. He could just rent it. That way he would need less money to start the learning centre and could do it sooner too. You think need more, but there are ways around it. You can solve the same problem differently and with less money.",
+    link: "https://amzn.to/3sW3qFH",
+    page: 83,
+  },
+  {
+    quote:
+      "There are many things never worth risking, no matter the potential gain. Reputation. Freedom and independence. Family and friends. Happiness. And your best shot at keeping these things is knowing when it's time to stop taking risks that might harm them. Knowing when you have enough.",
+    author: "Morgan Housel",
+    book: "The Psychology of Money",
+    link: "https://amzn.to/3sW3qFH",
+    page: 43,
+  },
+];
